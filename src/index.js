826603e2d5c1ca9bfdba9f67b4445b67ad62a2fd
larry <>
2023-05-11 18:05:13 +0800

    get git url

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const simpleGit = require('simple-git');
 const vscode = require('vscode');
+const { execSync } = require('child_process');
+const { dirname } = require('path');
+
 
 function activate(context) {
   // 创建 "Open RemoteRepository" 按钮
@@ -17,46 +20,6 @@ function activate(context) {
     vscode.window.showInformationMessage('gitbtn create by larryane!');
   });
 
-
-
-  // let openRepository = vscode.commands.registerCommand('gitbtn.openRepository', function () {
-	// 	// 获取当前打开的文件夹
-	// 	const repositoryUrl = 'https://juejin.cn/post/7226376235559403575'
-	// 	const currentFolder = vscode.workspace.workspaceFolders?.[0];
-	// 	const externalUri = vscode.Uri.parse(repositoryUrl);
-  // 	vscode.env.openExternal(externalUri);
-	// 	if (currentFolder) {
-	// 		// 使用 simple-git 打开 Git 仓库
-	// 		const git = simpleGit(currentFolder.uri.fsPath);
-	// 		console.log(11);
-	// 		const promise = new Promise((resolve, reject) => {
-	// 			git.outputHandler((command, stdout, stderr) => {
-	// 				stdout.on('data', (data) => {
-	// 					console.log(data.toString());
-	// 				});
-	// 				stderr.on('data', (data) => {
-	// 					console.error(data.toString());
-	// 				});
-	// 				stdout.on('end', () => {
-	// 					resolve();
-	// 				});
-	// 				stderr.on('end', () => {
-	// 					reject(new Error('Failed to open Git repository!'));
-	// 				});
-	// 			});
-	// 		});
-	
-	// 		promise.then((res) => {
-	// 			console.log(res,111);
-	// 			vscode.window.showInformationMessage('Git repository opened successfully!');
-	// 		}).catch((err) => {
-	// 			vscode.window.showErrorMessage(`Failed to open Git repository: ${err}`);
-	// 		});
-	// 	} else {
-	// 		vscode.window.showErrorMessage('No folder is opened in the workspace!');
-	// 	}
-	// });
-
 	let openRepository = vscode.commands.registerCommand('gitbtn.openRepository', async function () {
 		// 获取当前文件路径和行号
 		const editor = vscode.window.activeTextEditor;
@@ -67,25 +30,25 @@ function activate(context) {
 			vscode.window.showErrorMessage('No file is opened in the editor!');
 			return;
 		}
-	
-		// 获取Git仓库的根目录
-		const repositoryPath = await getRepositoryPath(filePath);
-		if (!repositoryPath) {
-			vscode.window.showErrorMessage('The current file is not in a Git repository!');
-			return;
+		function getGitRepositoryUrl(filePath) {
+			try {
+				// 执行 "git remote get-url origin" 命令获取 Git 仓库的 URL
+				const gitRepoUrl = execSync(`git remote get-url origin`, {
+					cwd: dirname(filePath),
+					encoding: 'utf-8',
+				}).trim();
+				return gitRepoUrl;
+			} catch (error) {
+				console.error(error);
+				return null;
+			}
 		}
+
+		const gitRepoUrl = getGitRepositoryUrl(filePath);
 	
-		// 获取 Git 仓库的 URL
-		const repositoryUrl = await getRepositoryUrl(repositoryPath);
-		if (!repositoryUrl) {
-			vscode.window.showErrorMessage('The current Git repository does not have a remote URL!');
-			return;
-		}
-	
-		// 将 Git 仓库 URL 转换为外部 URI，并在 URI 中包含当前文件和行号信息
-		const externalUri = await vscode.env.asExternalUri(vscode.Uri.parse(repositoryUrl));
-	
+		
 		// 打开外部 URI
+		const externalUri = await vscode.env.asExternalUri(vscode.Uri.parse(gitRepoUrl));
 		vscode.env.openExternal(externalUri);
 
 	});
