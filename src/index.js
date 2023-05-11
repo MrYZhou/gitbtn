a2c19ad492a7d17af24140134cdcc010a760f389
lg <>
2023-05-11 18:32:29 +0800

    publish info

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const simpleGit = require('simple-git');
+// const simpleGit = require('simple-git');
 const vscode = require('vscode');
 const { execSync } = require('child_process');
 const { dirname } = require('path');
 
-
+/**
+ * use ThemeIcon
+ */
 function activate(context) {
-  // 创建 "Open RemoteRepository" 按钮
+
   let initBtn = vscode.commands.registerCommand('gitbtn.initBtn', function () {
     const myButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Center, 100);
     myButton.tooltip = "Open RemoteRepository";
@@ -15,7 +17,6 @@ function activate(context) {
     myButton.show();
   });
 
-  // 创建 "gitbtnInfo" 命令
   let disposable = vscode.commands.registerCommand('gitbtn.gitbtnInfo', function () {
     vscode.window.showInformationMessage('gitbtn create by larryane!');
   });
@@ -45,16 +46,13 @@ function activate(context) {
 		}
 
 		const gitRepoUrl = getGitRepositoryUrl(filePath);
-	
-		
+
 		// 打开外部 URI
 		const externalUri = await vscode.env.asExternalUri(vscode.Uri.parse(gitRepoUrl));
 		vscode.env.openExternal(externalUri);
 
 	});
 	
-
-
   // 将命令注册到插件上下文中，以便在插件停用时自动注销
 	/**
 	 * Registering commands in the plugin context 
